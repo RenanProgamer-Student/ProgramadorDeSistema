4e2b252a2f0cfab5879d560deaf77c53b142be93
Renan.jav <96357423+RenanJaveiro@users.noreply.github.com>
2025-05-23 17:12:31 -0300

    Escola

---
+++ Escola/Escola/Form1.cs
@@ -0,0 +1,26 @@
+namespace Escola
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAcessar_Click(object sender, EventArgs e)
+        {
+            String user = tbxUser.Text;
+            String senha = tbxPass.Text;
+            String perfil = cbxPerfil.Text;
+
+            if (perfil == "Diretor")
+            {
+                F_Diretor f_Diretor = new F_Diretor(this);
+                f_Diretor.ShowDialog();
+                this.Close();
+                
+                
+            }
+        }
+    }
+}
